6af7655f1f9fd287e40e56b81a988137a7b01de5
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2019-11-28 21:00:04 +0530

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <memory>
 
 using namespace std;
 
@@ -18,6 +19,7 @@ int main () {
    cout << "Value of *ip variable: ";
    cout << *ip << endl;
 
+   // Memory Allocation using malloc, calloc, free have been deprecated since C++11
    int *arr;
 
    arr = (int * ) malloc(5 * sizeof(int));
@@ -26,6 +28,19 @@ int main () {
      arr[i] = i;
      std::cout << arr[i] << '\n';
    }
+
+   // Method currently in use for Memory Allocation
+
+   int *arr2 = new int[10];
+   delete[] arr2;
+
+   // Memory Allocation using allocator
+
+   allocator<int> alloc;
+   int *arr3 = alloc.allocate(10);
+   // use arr as you want to...
+   for(int i=0;i<10;i++) alloc.destroy(arr3+i);
+   alloc.deallocate(arr3, 10);
 
    return 0;
 }
